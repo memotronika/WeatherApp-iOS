ee22c6c2f937aa656064d7cf7855bb41260888c1
memotronika <memotronika@gmail.com>
2025-03-10 00:13:38 +0000

    swipe triggers added

--- WetherApp (Portfolio)/ViewController.swift
+++ WetherApp (Portfolio)/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var numberLabel: UILabel!
+    
+    @IBOutlet weak var InternalView: UIView!
+    
+    var internalViewPosition : CGRect!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        internalViewPosition = InternalView.frame
     }
 
-
+    @IBAction func PanOfInternalView(_ sender: UIPanGestureRecognizer) {
+        let translation = sender.translation(in: InternalView)
+        InternalView.center.x += translation.x
+        sender.setTranslation(.zero, in: InternalView)
+        if InternalView.frame.minX < -InternalView.frame.width / 7 {
+            InternalView.backgroundColor = .red
+        }
+        else if InternalView.frame.maxX > view.frame.width + InternalView.frame.width / 7 {
+            InternalView.backgroundColor = .green
+        }
+        if sender.state == .ended {
+            InternalView.frame = internalViewPosition
+        }
+    }
+    
 }
 
