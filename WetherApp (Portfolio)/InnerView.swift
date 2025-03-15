022575e75429021d98c4a367bd327713cffbbc07
memotronika <memotronika@gmail.com>
2025-03-15 12:13:15 +0000

    pan recognizer set up

---
+++ WetherApp (Portfolio)/InnerView.swift
@@ -0,0 +1,20 @@
+//
+//  InnerView.swift
+//  WetherApp (Portfolio)
+//
+//  Created by Anton Angel on 14/03/2025.
+//
+
+import UIKit
+
+class InnerView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
